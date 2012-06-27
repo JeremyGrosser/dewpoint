6d8c3d331906097a234aeafeb5dd3fb932e09ef1
Jeremy Grosser <jeremy@synack.me>
2012-06-26 17:26:27 -0700

    Update readme and whatnot for pypi

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
 from setuptools import setup
 
 setup(name='dewpoint',
+      description='urllib2 openers that sign requests for OAuth or Amazon APIs',
       version='0.1',
+      author='Jeremy Grosser',
+      author_email='jeremy+dewpoint@synack.me',
+      url='https://github.com/synack/dewpoint',
+      download_url='https://github.com/downloads/synack/dewpoint/dewpoint-0.1.tar.gz',
+      keywords=['urllib2', 'amazon', 'aws', 'oauth'],
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+      ],
       packages=['dewpoint'])
