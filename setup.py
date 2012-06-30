3e84ad1f089ca83745729c1ea0fc24ba7dcdcd5e
Jeremy Grosser <jeremy@synack.me>
2012-06-30 14:08:48 -0700

    Bump to 0.4.1

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(name='dewpoint',
       description='urllib2 openers that sign requests for OAuth or Amazon APIs',
-      version='0.3',
+      version='0.4.1',
       author='Jeremy Grosser',
       author_email='jeremy+dewpoint@synack.me',
       url='https://github.com/synack/dewpoint',
-      download_url='https://github.com/downloads/synack/dewpoint/dewpoint-0.3.tar.gz',
+      download_url='https://github.com/downloads/synack/dewpoint/dewpoint-0.4.1.tar.gz',
       keywords=['urllib2', 'amazon', 'aws', 'oauth'],
       classifiers=[
         'Development Status :: 3 - Alpha',
