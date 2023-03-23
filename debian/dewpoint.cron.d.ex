69561646718f9a0fe43e34bb66ef7f73d0e65a2d
Jeremy Grosser <jeremy@synack.me>
2023-03-23 15:30:04 +0000

    Moved debian directory to debian branch

--- debian/dewpoint.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the dewpoint package
-#
-0 4	* * *	root	[ -x /usr/bin/dewpoint_maintenance ] && /usr/bin/dewpoint_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(dewpoint):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="dewpoint" command="/usr/bin/dewpoint"
