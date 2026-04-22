47ab4285b0fc92c3f5557d5dd97ea8048238a205
Jeremy Grosser <jeremy@synack.me>
2026-04-22 14:05:49 -0700

    Remove deprecated AWSAuthHandlerV4

--- dewpoint/aws.py
+++ dewpoint/aws.py
@@ -67,61 +67,6 @@ def canonical_hash(req):
     return hashlib.sha256(canon.encode('utf8')).hexdigest()
 
 
-# Will be deprecated on 2026-01-31, migrate to OAuth2Handler
-class AWSAuthHandlerV4(urllib.request.BaseHandler):
-    def __init__(self, key, secret, region, service, timeout=None):
-        self.key = key
-        self.secret = secret
-        self.region = region
-        self.service = service
-        self.timeout = timeout
-
-    def signing_key(self, scope):
-        key = b'AWS4' + self.secret.encode('utf8')
-        for msg in scope.split('/'):
-            key = hmac.digest(key, msg.encode('utf8'), hashlib.sha256)
-        return key
-
-    def sign(self, req):
-        canon_hash = canonical_hash(req)
-        scope = '{date}/{region}/{service}/aws4_request'.format(
-            date=format_date(req.timestamp),
-            region=self.region,
-            service=self.service)
-
-        signing_key = self.signing_key(scope)
-
-        string_to_sign = '\n'.join([
-            'AWS4-HMAC-SHA256',
-            format_time(req.timestamp),
-            scope,
-            canon_hash,
-        ])
-        string_to_sign = string_to_sign.encode('utf8')
-        signature = hmac.digest(signing_key, string_to_sign, hashlib.sha256)
-
-        req.add_header('Authorization', 'AWS4-HMAC-SHA256 Credential={key}/{scope}, SignedHeaders={signed_headers}, Signature={signature}'.format(
-            key=self.key,
-            scope=scope,
-            signed_headers=canonical_headers(req).rsplit('\n', 1)[1],
-            signature=signature.hex()))
-
-    def http_request(self, req):
-        req.timestamp = time.gmtime(time.time())
-        if 'Host' not in req.headers:
-            req.add_header('Host', req.host)
-        if 'x-amz-date' not in req.headers:
-            req.add_header('x-amz-date', format_time(req.timestamp))
-
-        self.sign(req)
-
-        req.timeout = self.timeout
-        return req
-
-    def https_request(self, req):
-        return self.http_request(req)
-
-
 class OAuth2Handler(urllib.request.BaseHandler):
     def __init__(self,
                  client_id,
