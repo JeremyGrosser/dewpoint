a2243df1341e212a2e6538ec6461a590175db8fd
Jeremy Grosser <jeremy@synack.me>
2026-04-22 15:21:48 -0700

    lint, more dead code removal

--- dewpoint/aws.py
+++ dewpoint/aws.py
@@ -2,69 +2,8 @@ import urllib.parse
 import urllib.request
 import urllib.error
 import logging
-
-import hashlib
-import hmac
 import time
 import json
-
-
-def format_time(ts):
-    return time.strftime('%Y%m%dT%H%M%SZ', ts)
-
-
-def format_date(ts):
-    return time.strftime('%Y%m%d', ts)
-
-
-def parse_uri(req):
-    method = req.get_method()
-    if method == b'POST':
-        query = req.data
-        uri = req.selector
-    else:
-        s = req.selector
-        if s.find('?') != -1:
-            uri, query = s.split('?', 1)
-        else:
-            uri = s
-            query = ''
-    return method, uri, query
-
-
-def canonical_headers(req):
-    keys = []
-    headers = []
-    for key, value in sorted(req.headers.items()):
-        key = key.strip().lower()
-        value = value.strip()
-        headers.append('%s:%s' % (key, value))
-        keys.append(key)
-    canon = '\n'.join(headers) + '\n\n' + ';'.join(keys)
-    return canon
-
-
-def canonical_hash(req):
-    method, uri, query = parse_uri(req)
-
-    query = urllib.parse.parse_qsl(query)
-    query.sort(key=lambda x: x[0])
-    query = urllib.parse.urlencode(query)
-
-    headers = canonical_headers(req)
-
-    if req.data is not None:
-        payload = req.data
-    else:
-        payload = b''
-
-    canon = '{method}\n{uri}\n{query}\n{headers}\n'.format(
-        method=method,
-        uri=uri,
-        query=query,
-        headers=headers)
-    canon += hashlib.sha256(payload).hexdigest()
-    return hashlib.sha256(canon.encode('utf8')).hexdigest()
 
 
 class OAuth2Handler(urllib.request.BaseHandler):
@@ -131,7 +70,6 @@ class OAuth2Handler(urllib.request.BaseHandler):
             )
         else:
             authz = 'Bearer {access_token}'.format(
-                token_type=token['token_type'],
                 access_token=token['access_token']
             )
         req.add_header('Authorization', authz)
