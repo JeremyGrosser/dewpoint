47f519d124ac792142c4728e17a798138a411957
Jeremy Grosser <jeremy@synack.me>
2026-04-22 14:10:01 -0700

    Remove dead code/examples, release 2.0.0

--- README.md
+++ README.md
@@ -1,25 +1,2 @@
 # dewpoint
 urllib openers that sign requests for Amazon APIs
-
-## Amazon Web Services
-
-```python
-from pprint import pprint
-import os
-
-import dewpoint.aws
-
-
-ec2 = dewpoint.aws.AWSProxy(
- 	key=os.environ['AWS_ACCESS_KEY_ID'],
- 	secret=os.environ['AWS_SECRET_ACCESS_KEY'],
- 	version='2012-06-01',
- 	endpoint='https://ec2.us-east-1.amazonaws.com')
-
-
-response = ec2.DescribeInstances()
-# response is an xml.etree.ElementTree.Element instance
-for reservation in response.iterfind('reservationSet/item'):
- 	for instance in reservation.iterfind('instancesSet/item'):
-  	pprint(dewpoint.aws.dictwalk(instance))
-```
--- setup.py
+++ setup.py
@@ -2,15 +2,14 @@ from setuptools import setup
 
 setup(name='dewpoint',
       description='urllib openers that sign requests for Amazon APIs',
-      version='1.1.0',
+      version='2.0.0',
       author='Jeremy Grosser',
       author_email='jeremy+dewpoint@synack.me',
       url='https://github.com/JeremyGrosser/dewpoint',
-      download_url='https://github.com/JeremyGrosser/dewpoint/archive/1.0.1.tar.gz',
+      download_url='https://github.com/JeremyGrosser/dewpoint/archive/2.0.0.tar.gz',
       keywords=['urllib', 'amazon', 'aws'],
       classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
       ],
       packages=['dewpoint'])
